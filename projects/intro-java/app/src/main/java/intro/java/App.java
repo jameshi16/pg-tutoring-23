a496d92b18c8c25fc998cf762e3fc9762d90791b
James <lim.en.hui.james@gmail.com>
2024-01-12 11:08:17 +0000

    Update Week 12

--- projects/intro-java/app/src/main/java/intro/java/App.java
+++ projects/intro-java/app/src/main/java/intro/java/App.java
@@ -32,6 +32,42 @@ public class App {
         System.out.println(a == b);
     }
 
+    public static void m_5() {
+        String a = "some_string";
+        String b = "another_string";
+
+        System.out.println(a + b);
+        System.out.println(a.concat(b));
+    }
+
+    public static void m_6() {
+        StringBuilder a = new StringBuilder("some_string");
+        a.append("another_string");
+
+        System.out.println(a.toString());
+    }
+
+    public static void m_7(Integer notANormalInt) {
+        System.out.println(notANormalInt);
+    }
+
+    public static void m_8() {
+        Something a = new Something();
+        Something b = new Something();
+        b.b = 100;
+        System.out.println("Printing a");
+        a.print_all();
+        System.out.println("Printing b");
+        b.print_all();
+
+        Something.change_a();
+
+        System.out.println("After changing, printing a");
+        a.print_all();
+        System.out.println("After changing, printing b");
+        b.print_all();
+    }
+
     public static void scratch() {
     }
 
@@ -39,7 +75,10 @@ public class App {
         // m_1();
         // m_2();
         // m_3();
-        m_4();
+        // m_4();
+        // m_5();
+        // m_6();
+        m_7(123);
         // scratch();
     }
 }
